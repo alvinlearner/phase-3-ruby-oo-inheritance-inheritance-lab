6fc3b2f8a49150b2d9eede2f95e743bcaeebb0af
alvinkenyagah <alvin47k@gmail.com>
2023-05-26 15:01:54 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,29 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize(knowledge = [] )
+
+        @knowledge = knowledge
+    end
+
+    def learn(new_knowledge)
+
+        @knowledge << new_knowledge
+
+    end
+
+    def show_knowledge
+        @knowledge
+    end
 
 end
+
+
+snoop = Student.new
+
+snoop.learn("maths")
+
+puts snoop.show_knowledge
